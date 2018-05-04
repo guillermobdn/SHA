f5147950c3f32832e8f3a68c8227aff2b45e016f
guillermobdn <guillermopulidofernandez@gmail.com>
2018-05-04 16:21:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/sha/CameraInfoActivity.java
@@ -0,0 +1,34 @@
+package com.example.android.sha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CameraInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_info);
+
+        TextView tvInfo = findViewById(R.id.info);
+        Button test = findViewById(R.id.test);
+
+        //VideoProfile.CameraCapabilities
+
+        tvInfo.setText(Html.fromHtml("<p><b>Fabricante:</b>Sony</p><p><b>Megapixels</b>: 1000 Mpx</p>"));
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CameraInfoActivity.this, CameraTestActivity.class));
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/android/sha/CameraTestActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.sha;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CameraTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_test);
+    }
+}
